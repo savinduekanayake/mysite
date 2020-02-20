dfda45ad3197dde90887df652dd364d569003d24
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-02-21 02:05:48 +0530

    model test, viewtest implimented

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, Http404 , HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+
 
 from .models import Choice, Question
 
@@ -10,8 +12,11 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        # __lte=>less than or equal to
 
 class DetailView(generic.DetailView):
     model = Question
@@ -21,6 +26,12 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
@@ -40,6 +51,19 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
